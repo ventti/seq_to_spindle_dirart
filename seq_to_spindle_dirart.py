9d97a0e5911871df8bc3ab2408d4f7c967d50a01
ventti <11227794+ventti@users.noreply.github.com>
2022-09-24 22:28:05 +0300

    Added the script

---
+++ seq_to_spindle_dirart.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# dirart.seq to spindle textfile
+import sys
+
+try:
+	fname = sys.argv[1]
+	with open(fname) as fp:
+		data = fp.read()
+		data = data[1:]
+except:
+	print(f"usage: {sys.argv[0]} filename.seq [linelen=16]")
+try:
+	linelen = int(sys.argv[2])
+except:
+	linelen = 16  # dir art
+
+
+i = 0
+while i < len(data):
+	print(data[i:i+linelen])# + f"{i}:{i+linelen}")
+	i += linelen
+
